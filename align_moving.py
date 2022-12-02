63114b2933d2c6a2bb62fafc0c529d21aac5fdff
Marckie Zeender <mkzeender@gmail.com>
2022-12-01 21:18:49 -0800

    added aperture location finding

--- align_moving.py
+++ align_moving.py
@@ -8,6 +8,28 @@ from astropy.io import fits
 
 from pathlib import Path
 from dateutil.parser import parse
+import mplcursors
+
+
+
+def get_coords_of_point(image, vmin=0, vmax=255):
+    with plt.ion():
+        with fits.open(image) as img_open:
+            img = img_open[0].data
+            t = img_open[0].header['DATE-OBS']
+
+            def print_coords(sel):
+                x, y = sel.target
+                print(f'{x}, {y}, {repr(t)}')
+                sel.annotation.set_text(f'{sel.target[0]}, {sel.target[1]}, {repr(t)}')
+
+            fig, ax = plt.subplots()
+
+            _im = ax.imshow(img, cmap='gray', vmin=vmin, vmax=vmax, origin='lower')
+
+            mplcursors.cursor(_im).connect("add", print_coords)
+
+            plt.show(block=True)
 
 
 
@@ -22,7 +44,9 @@ def do_photometry_with_timestamps(
         aperture_=15,
         annulus_inner=25,
         annulus_outer=45,
-        dry_run=False
+        dry_run=False,
+
+        reference_positions=[]
         ):
     images = list(images)
     
@@ -36,7 +60,10 @@ def do_photometry_with_timestamps(
     velocity_y = deltay_total / delta_t_total
 
     photometry_target = pd.DataFrame(columns = ["num", "id", "xcenter", "ycenter", "aperture_sum", "total_bkg", "aperture_sum_bkgsub"])
-    photometry_star = pd.DataFrame(columns = ["num", "id", "xcenter", "ycenter", "aperture_sum", "total_bkg", "aperture_sum_bkgsub"])
+    photometry_star_list = [
+        pd.DataFrame(columns = ["num", "id", "xcenter", "ycenter", "aperture_sum", "total_bkg", "aperture_sum_bkgsub"])
+        for _ in reference_positions
+    ]
 
     for i, img_path in enumerate(images):
         img_open = fits.open(Path(img_path))
@@ -49,8 +76,8 @@ def do_photometry_with_timestamps(
         object_position_y = start_object_y + velocity_y * delta_t_obj
         
         #target_position = (348, 971)
-        #positions = [target_position, (object_position_x, object_position_y)]
-        positions = [(object_position_x, object_position_y)] # ADD OTHERS?
+        positions = reference_positions + [(object_position_x, object_position_y)]
+        # positions = [(object_position_x, object_position_y)] # ADD OTHERS?
 
         aperture = CircularAperture(positions, r = aperture_)
         annulus_aperture = CircularAnnulus(positions, r_in = annulus_inner, r_out = annulus_outer)
@@ -58,9 +85,9 @@ def do_photometry_with_timestamps(
         if dry_run:
             if not i % (len(images) // 5):
                 fig, ax = plt.subplots()
-                ax.invert_yaxis()
 
-                ax.imshow(img, cmap='gray', vmin=0, vmax=255)
+
+                ax.imshow(img, cmap='gray', vmin=0, vmax=255, origin='upper')
                 aperture.plot(ax, color='red', lw=1, label='Photometry aperture')
                 annulus_aperture.plot(ax, color='green', lw=1, label='Photo annulus')
 
@@ -78,16 +105,26 @@ def do_photometry_with_timestamps(
             for col in phot_table.colnames:
                 phot_table[col].info.format = '%.8g'  # for consistent table output
             
+
             phot_dataframe = pd.DataFrame(np.array(phot_table))
-            photometry_target.loc[i] = phot_dataframe.iloc[0]
-            # photometry_star.loc[i] = phot_dataframe.iloc[1]
+            for j in range(len(reference_positions)):
+                photometry_star_list[j].loc[i] = phot_dataframe.iloc[j]
+            photometry_target.loc[i] = phot_dataframe.iloc[j+1]
         ## add more here if there are more than 2 apertures
-        
-    combined_data = pd.DataFrame(columns = ["target_subtracted_counts", "object_subtracted_counts", "num"])
+
+    photometry_star_mean = pd.DataFrame(columns = ["num", "id", "xcenter", "ycenter", "aperture_sum", "total_bkg", "aperture_sum_bkgsub"])
+
+    # calculate the mean values for all the stars
+    for data_frame in photometry_star_list:
+        photometry_star_mean += data_frame
+    
+    photometry_star_mean /= len(photometry_star_list)
+    
+    combined_data = pd.DataFrame(columns = ["target_subtracted_counts", "star_subtracted_counts", "num"])
     combined_data["target_subtracted_counts"] = photometry_target["aperture_sum_bkgsub"]
-    combined_data["object_subtracted_counts"] = photometry_star["aperture_sum_bkgsub"]
-    combined_data["differential"] = combined_data["object_subtracted_counts"] - combined_data["target_subtracted_counts"]
-    combined_data["uncalibrated_object_mag"] = -2.5 * np.log10(combined_data["object_subtracted_counts"])
+    combined_data["star_subtracted_counts"] = photometry_star_mean["aperture_sum_bkgsub"]
+    combined_data["differential"] = combined_data["star_subtracted_counts"] - combined_data["target_subtracted_counts"]
+    combined_data["uncalibrated_object_mag"] = -2.5 * np.log10(combined_data["star_subtracted_counts"])
     combined_data["uncalibrated_target_mag"] = -2.5 * np.log10(combined_data["target_subtracted_counts"])
     combined_data["differential_mag"] = combined_data["uncalibrated_object_mag"] - combined_data["uncalibrated_target_mag"]
     for i in range(len(combined_data)):
